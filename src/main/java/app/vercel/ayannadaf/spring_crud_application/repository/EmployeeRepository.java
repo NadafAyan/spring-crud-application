df0e0f28dcb5c6ed6a00a8922c00575ef445cf34
Ayan Nadaf <nadafayan95@gmail.com>
2026-06-30 22:30:11 +0530

    Updated application.properties to use MySQL Database

--- src/main/java/app/vercel/ayannadaf/spring_crud_application/repository/EmployeeRepository.java
+++ src/main/java/app/vercel/ayannadaf/spring_crud_application/repository/EmployeeRepository.java
@@ -1,4 +1,8 @@
 package app.vercel.ayannadaf.spring_crud_application.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class EmployeeRepository {
+
 }
--- src/main/java/app/vercel/ayannadaf/spring_crud_application/service/EmployeeService.java
+++ src/main/java/app/vercel/ayannadaf/spring_crud_application/service/EmployeeService.java
@@ -1,8 +1,19 @@
 package app.vercel.ayannadaf.spring_crud_application.service;
 
+import app.vercel.ayannadaf.spring_crud_application.entity.Employee;
+import app.vercel.ayannadaf.spring_crud_application.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+    private EmployeeRepository employeeRepository;
 
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+//    public Employee createEmployee(Employee employeeReq) {
+//        // Business Logic
+//        Employee employeeResp = employeeRepository
+//    }
 }
